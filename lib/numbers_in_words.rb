15ee392315398414ab1e7e543777c5eff2deb2ff
Justin Pacubas & Josephine Wright <student@epicodus.com>
2014-07-29 14:27:13 -0700

    Everything is working except for the hyphens.

--- lib/numbers_in_words.rb
+++ lib/numbers_in_words.rb
@@ -1,32 +1,74 @@
 def numbers_in_words(input)
-  input_array = input.to_s.split('').map { |digit| digit.to_i }
+  input_s_array = input.split(',')
 
-  tweens = {1 => 'one', 2 => 'two', 3 => 'three', 4 => 'four', 5 => 'five', 6 => 'six', 7 => 'seven', 8 => 'eight', 9 => 'nine', 10 => 'ten',
-             11 => 'eleven', 12 => 'twelve', 13 => 'thirteen', 14 => 'fourteen', 15 => 'fifteen', 16 => 'sixteen', 17 => 'seventeen', 18 => 'eighteen', 19 => 'nineteen'}
-  tens = {2 => 'twenty', 3 => 'thirty', 4 => 'forty', 5 => 'fifty', 6 => 'sixty', 7 => 'seventy', 8 => 'eighty', 9 => 'ninety'}
-  result = ''
-  p input_array
+  ones = {'1' => 'one', '2' => 'two', '3' => 'three', '4' => 'four', '5' => 'five', '6' => 'six', '7' => 'seven', '8' => 'eight', '9' => 'nine'}
+  teens = {'10' => 'ten', '11' => 'eleven', '12' => 'twelve', '13' => 'thirteen', '14' => 'fourteen', '15' => 'fifteen', '16' => 'sixteen', '17' => 'seventeen', '18' => 'eighteen', '19' => 'nineteen'}
+  tens = {'2' => 'twenty', '3' => 'thirty', '4' => 'forty', '5' => 'fifty', '6' => 'sixty', '7' => 'seventy', '8' => 'eighty', '9' => 'ninety'}
 
-  if input_array.length == 2 && input_array[0] != 1
-    tens.each do  |key, val|
-      if input_array[0] == key
-        result += val
+  input_arr = []
+  result = ""
+
+  input_s_array.each do  |threes|
+    input_arr << threes
+  end
+
+  store_length = input.length
+
+  input_arr.each do  |threes|
+    if threes.length > 2
+      ones.each do  |key, val|
+        if threes[0] == key
+          result += val + " "
+        end
+      end
+      if threes.length == 3 && threes[0] >= '1'
+        result += "hundred" + " "
+      end
+      threes.slice!(0)
+    end
+
+    if threes.length == 2 && threes[0] != '1'
+      tens.each do  |key, val|
+        if threes[0] == key && threes[1] != 0
+          result += val + "-"
+        end
+      end
+      ones.each do  |key, val|
+        if threes[1] == key
+          result += val + " "
+        end
+      end
+    elsif threes.length == 2 && threes[0] == '1'
+      teens.each do  |key, val|
+        if threes == key
+          result += val + " "
+        end
+      end
+    elsif threes.length == 1
+      ones.each do  |key, val|
+        if threes == key
+          result += val + " "
+        end
       end
     end
-    tweens.each do  |key, val|
-      if input_array[1] == key
-        result += val
-      end
-    end
-  elsif input_array.length == 2 && input_array[0] == 1
-    tweens.each do  |key, val|
-      if input_array.join.to_i == key
-        result += val
-      end
+    if store_length == 17
+      result += 'trillion '
+      store_length -=3
+    elsif store_length >= 13 && store_length <= 15
+      result += 'billion '
+      store_length -=3
+    elsif store_length >= 9 && store_length <= 11
+      result += 'million '
+      store_length -=4
+      p store_length
+    elsif store_length >= 5 && store_length <= 7
+      result += 'thousand '
+      store_length -=3
     end
   end
-  p result
-end
 
-numbers_in_words(99)
+result.slice!(result.length-1)
+p result
+
+end
 
--- spec/numbers_in_words_spec.rb
+++ spec/numbers_in_words_spec.rb
@@ -3,14 +3,26 @@ require ('numbers_in_words')
 
 describe('numbers_in_words') do
   it('returns ten for the input of 10') do
-    numbers_in_words(10).should(eq('ten'))
+    numbers_in_words("10").should(eq("ten"))
   end
 
   it('returns fourteen for the input of 14') do
-    numbers_in_words(14).should(eq('fourteen'))
+    numbers_in_words("14").should(eq("fourteen"))
   end
 
   it('returns twenty nine for the input of 29') do
-    numbers_in_words(29).should(eq('twentynine'))
+    numbers_in_words("29").should(eq("twenty-nine"))
+  end
+
+  it('returns three hundred forty six for the input of 346') do
+    numbers_in_words("346").should(eq("three hundred forty-six"))
+  end
+
+  it('returns ten thousand thirty four for the input of 1034') do
+    numbers_in_words("10,034").should(eq("ten thousand thirty-four"))
+  end
+
+  it('returns one billion three hundred million seventy thousand seven for the input of 1300070007') do
+    numbers_in_words("1,300,070,007").should(eq("one billion three hundred million seventy thousand seven"))
   end
 end
